e1f6e1c120f3a38dfc6539490e45768ccc30d1e3
Abhinav Sharma <abhinav.ih@gmail.com>
2016-09-27 13:33:40 +0530

    geofence code added for button clicks

--- app/src/main/java/com/abhinav/geofence/HomeActivity.java
+++ app/src/main/java/com/abhinav/geofence/HomeActivity.java
@@ -1,16 +1,153 @@
 package com.abhinav.geofence;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by abhinav.sharma on 9/26/2016.
  */
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String HOME_GEOFENCE = "A-97,Sector 55, NOIDA";
+    private Button startLocationMonitoring, startGeofencingMonitoring, stopGeofencingMonitoring;
+    private GoogleApiClient apiClient = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setupUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int response = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+        if (response != ConnectionResult.SUCCESS) {
+            GoogleApiAvailability.getInstance().getErrorDialog(this, response, 1).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        apiClient.disconnect();
+    }
+
+    private void setupUI() {
+
+        startLocationMonitoring = (Button) findViewById(R.id.btn1);
+        startGeofencingMonitoring = (Button) findViewById(R.id.btn2);
+        stopGeofencingMonitoring = (Button) findViewById(R.id.btn3);
+
+        startLocationMonitoring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocationRequest locationRequest = LocationRequest.create()
+                        .setInterval(5000)
+                        .setFastestInterval(5000)
+                        .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 272);
+                    }
+                    return;
+                }
+                LocationServices.FusedLocationApi.requestLocationUpdates(apiClient, locationRequest, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.e("onLocationChanged: ", "Location is: " + location.toString());
+                        Log.e("Lat Long: ", location.getLatitude() + " " + location.getLongitude());
+                    }
+                });
+            }
+        });
+
+        startGeofencingMonitoring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Geofence geofence = new Geofence.Builder()
+                        .setRequestId(HOME_GEOFENCE)
+                        .setCircularRegion(28.606181, 77.346082, 100.0f)
+                        .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                        .setNotificationResponsiveness(1000)
+                        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT | Geofence.GEOFENCE_TRANSITION_DWELL)
+                        .build();
+
+                GeofencingRequest geofencingRequest = new GeofencingRequest.Builder()
+                        .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                        .addGeofence(geofence).build();
+            }
+        });
+
+        stopGeofencingMonitoring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        Log.e("GoogleApiClient", "onConnected: ");
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.e("GoogleApiClient", "onConnectionSuspended: ");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.e("GoogleApiClient", "onConnectionFailed: ");
+                    }
+                })
+                .build();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 272);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // TODO -- use case for permission not given is not handled yet .. Be careful !!
+        if(requestCode == 272
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+
+        }
     }
 }
